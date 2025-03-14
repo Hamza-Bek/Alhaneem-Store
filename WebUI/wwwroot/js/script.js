bb81f4efcdb229c3c00f2f77b2a7b0293718f667
Hamza Bek <hamzamrbek@gmail.com>
2025-03-14 22:33:51 +0200

    Add image slider component

---
+++ WebUI/wwwroot/js/script.js
@@ -0,0 +1,33 @@
+new Swiper('.card-wrapper', {
+    autoplay: {
+        delay: 2000, // Time in milliseconds
+        disableOnInteraction: false,
+    },
+    loop: true,
+    spaceBetween: 30,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints : {
+        0: {
+            slidesPerView : 1
+        },
+        768: {
+            slidesPerView : 1
+        },
+        1024: {
+            slidesPerView : 1
+        }
+    }
+});
