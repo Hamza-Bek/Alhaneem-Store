3e48ed17551897f93a9fd2607861a1ca8ce18ce3
Hamza Bek <hamzamrbek@gmail.com>
2025-04-15 07:03:52 +0200

    remove unused properties from cart item dto

--- Application/Dtos/Cart/CartItemDto.cs
+++ Application/Dtos/Cart/CartItemDto.cs
@@ -4,12 +4,5 @@ public class CartItemDto
 {
     // Quantity Information
     public int Quantity { get; set; } 
-    
-    // Pricing Information
-    public decimal Price { get; set; }
-    public decimal TotalPrice { get; set; }
-
-    //Foreign Keys & Navigation Properties
-    public Guid CartId { get; set; }
     public Guid ProductId { get; set; } 
 }
