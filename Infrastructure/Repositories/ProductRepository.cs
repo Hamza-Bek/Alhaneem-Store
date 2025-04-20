cb03cd43ee2132ad50bc622f7e10c0d9cff6bff3
Hamza Bek <hamzamrbek@gmail.com>
2025-04-20 21:32:50 +0200

    refactors : make methods return productDto + remove admin methods

--- Infrastructure/Repositories/ProductRepository.cs
+++ Infrastructure/Repositories/ProductRepository.cs
@@ -1,4 +1,6 @@
+using Application.Dtos.Product;
 using Application.Interfaces;
+using Application.Mappers;
 using Application.Options;
 using Domain.Enums;
 using Domain.Models;
@@ -18,12 +20,10 @@ public class ProductRepository : IProductRepository
         _context = context;
     }
 
-    public async Task<IEnumerable<Product>> GetProductsAsync()
+    public async Task<IEnumerable<PublicProductDto>> GetProductsAsync()
     {
-        var products = await _context.Products
-            .ToListAsync();
-
-        return products;
+        var products = await _context.Products.ToListAsync();
+        return products.Select(p => p.ToPublicDto());
     }
 
     public Task<Product> GetProductByIdAsync(Guid id)
@@ -32,65 +32,5 @@ public class ProductRepository : IProductRepository
             .FirstOrDefaultAsync(p => p.Id == id);
 
         return product;
-    }
-
-    public async Task<Product> CreateProductAsync(Product product)
-    {
-        var userId = _userIdentity.Id;
-        
-        if(userId == Guid.Empty)
-            throw new UnauthorizedAccessException($"User is not authenticated. {userId}");
-
-        var newProduct = new Product()
-        {
-            Id = Guid.NewGuid(),
-            Name = product.Name,
-            Description = product.Description,
-            CategoryId = product.CategoryId,
-            Price = product.Price,
-            Cost = product.Cost,
-            Stock = product.Stock,
-            CreatedAt = DateTime.UtcNow,
-            ModifiedAt = DateTime.UtcNow,
-            StockStatus = StockStatus.InStock,
-        };
-        
-        _context.Products.Add(newProduct);
-        await _context.SaveChangesAsync();
-        
-        return newProduct;
-    }
-
-    public async Task<Product> UpdateProductAsync(Guid id, Product product)
-    {
-        var productToUpdate = await _context.Products
-            .FirstOrDefaultAsync(p => p.Id == id);
-
-        if (productToUpdate == null)
-            return null;
-        
-        productToUpdate.Name = product.Name;
-        productToUpdate.Description = product.Description;
-        productToUpdate.CategoryId = product.CategoryId;
-        productToUpdate.Price = product.Price;
-        productToUpdate.Cost = product.Cost;
-        productToUpdate.Stock = product.Stock;
-        productToUpdate.StockStatus = product.StockStatus;
-        productToUpdate.ModifiedAt = DateTime.UtcNow;
-        
-        await _context.SaveChangesAsync();
-
-        return productToUpdate;
-    }
-
-    public async Task<bool> DeleteProductAsync(Guid id)
-    {
-        var product = await _context.Products.FindAsync(id);
-        if (product is null)
-            return false;
-
-        _context.Products.Remove(product);
-        await _context.SaveChangesAsync();
-        return true;
     }
 }
