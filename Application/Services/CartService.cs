1d30b1b67d503e6a298556e739f56f6b93bc9802
Hamza Bek <hamzamrbek@gmail.com>
2025-04-20 17:27:39 +0200

    feature : add cart client service

--- Application/Services/CartService.cs
+++ Application/Services/CartService.cs
@@ -13,12 +13,32 @@ public class CartService
 
     public async Task<bool> CreateCartAsync(string sessionId)
     {
-        var response = await _httpClient.PostAsJsonAsync("api/carts/create", sessionId);
-        if (response.IsSuccessStatusCode)
+        var response = await _httpClient.PostAsJsonAsync("api/carts/cart", sessionId);
+        return response.IsSuccessStatusCode;
+    }
+    
+    public async Task<bool> AddItemToCartAsync(Guid productId, int quantity, string sessionId)
+    {
+        var payload = new
         {
-            return true;
-        }
+            ProductId = productId,
+            Quantity = quantity,
+            SessionId = sessionId
+        };
 
-        return false;
+        var response = await _httpClient.PostAsJsonAsync("api/carts/item", payload);
+        return response.IsSuccessStatusCode;
+    }
+    
+    public async Task<bool> RemoveItemFromCartAsync(Guid productId, string sessionId)
+    {
+        var payload = new
+        {
+            ProductId = productId,
+            SessionId = sessionId
+        };
+
+        var response = await _httpClient.PostAsJsonAsync("api/carts/remove/item", payload);
+        return response.IsSuccessStatusCode;
     }
 }
--- Application/Services/ICartService.cs
+++ Application/Services/ICartService.cs
@@ -3,4 +3,5 @@ namespace Application.Services;
 public interface ICartService
 {
     Task<bool> CreateCartAsync(string sessionId);
+    Task<bool> AddItemToCartAsync(Guid productId, int quantity, string sessionId);
 }
