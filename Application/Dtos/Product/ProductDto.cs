ac940bb4fc1c6433de64f7309be2da304ec781a1
Hamza Bek <hamzamrbek@gmail.com>
2025-04-20 21:31:34 +0200

    refactor : add product id

--- Application/Dtos/Product/ProductDto.cs
+++ Application/Dtos/Product/ProductDto.cs
@@ -5,9 +5,10 @@ namespace Application.Dtos.Product;
 
 public class ProductDto
 {
+    public Guid Id { get; set; }
     public string Name { get; set; }
     public string? Description { get; set; }
-    public Guid? CategoryId { get; set; }
+    public string CategoryId { get; set; }
     public decimal Price { get; set; }
     public decimal? Cost { get; set; }
     public int Stock { get; set; }
