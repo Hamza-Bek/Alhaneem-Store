85cd7f2dee64b20993b12a8ec929889ee2d4f301
Hamza Bek <hamzamrbek@gmail.com>
2025-05-02 23:38:05 +0300

    refactor : change the ICartService Interface to accept update quantity method

--- Application/Services/ICartService.cs
+++ Application/Services/ICartService.cs
@@ -1,7 +1,12 @@
+using Application.Dtos.Cart;
+using Domain.Models;
+
 namespace Application.Services;
 
 public interface ICartService
 {
+    Task<CartDto> GetUserCartBySessionIdAsync(string sessionId);
     Task<bool> CreateCartAsync(string sessionId);
-    Task<bool> AddItemToCartAsync(Guid productId, int quantity, string sessionId);
+    Task<CartDto?> UpdateItemQuantityAsync(Guid productId, int quantityDelta, string sessionId);
+    Task<CartDto?> RemoveItemAsync(Guid productId, string sessionId);
 }
