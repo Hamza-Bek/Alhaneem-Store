0c9aaff87a440076e9bc6679264907007e5cdfea
Hamza Bek <hamzamrbek@gmail.com>
2025-05-04 19:33:10 +0300

    add CartMapper & OrderMapper

---
+++ Application/Mappers/CartMapper.cs
@@ -0,0 +1,28 @@
+﻿using Application.Dtos.Cart;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mappers;
+public static class CartMapper
+{
+    public static CartDto ToDto(this Cart cart)
+    {
+        return new CartDto
+        {
+            Id = cart.Id,
+            Subtotal = cart.Subtotal,
+            ShippingFee = cart.ShippingFee,
+            DiscountAmount = cart.DiscountAmount,
+            Total = cart.Total,
+            IsCheckedOut = cart.IsCheckedOut,
+            CreatedAt = cart.CreatedAt,
+            SessionId = cart.SessionId,
+            Items = cart.Items?.Select(i => i.ToDto()).ToList() ?? new List<CartItemDto>()
+        };
+    }
+}
+
---
+++ Application/Mappers/OrderMapper.cs
@@ -0,0 +1,41 @@
+﻿using Application.Dtos.Order;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mappers
+{
+    public static class OrderMapper
+    {
+        public static OrderDto ToDto(this Order order)
+        {
+            return new OrderDto
+            {
+                Id = order.Id,
+                OrderNumber = order.OrderNumber,
+                OrderStatus = order.OrderStatus.ToString(),
+                DeliveryStatus = order.DeliveryStatus.ToString(),
+                PaymentStatus = order.PaymentStatus.ToString(),
+                CreatedAt = order.CreatedAt,
+                Subtotal = order.Subtotal,
+                ShippingFee = order.ShippingFee,
+                DiscountAmount = order.DiscountAmount,
+                Total = order.Total,
+
+                Items = order.Items.Select(item => new OrderItemDto
+                {
+                    ProductId = item.ProductId,
+                    ProductName = item.Product?.Name ?? string.Empty,                    
+                    ImageUrls = item.Product.Images?.Select(i => i.ImageUrl).ToList() ?? new (),
+                    Quantity = item.Quantity,
+                    Price = item.Price,
+                    TotalPrice = item.TotalPrice
+                }).ToList(),
+                Location = order.Location?.ToDto()
+            };
+        }
+    }
+}
