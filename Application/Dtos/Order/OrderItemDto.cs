1efe49bf3939f34d82b9bbc0673b3f9553996525
Hamza Bek <hamzamrbek@gmail.com>
2025-05-04 19:32:52 +0300

    Create ItemDto

---
+++ Application/Dtos/Order/OrderItemDto.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos.Order
+{
+    public class OrderItemDto
+    {
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public List<string> ImageUrls { get; set; } = new();
+
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
