f7fa887681eee53de6a6722ce6b17af52b13e7a3
Hamza Bek <hamzamrbek@gmail.com>
2025-05-04 19:29:09 +0300

    remove navigation props from user model

--- Domain/Models/ApplicationUser.cs
+++ Domain/Models/ApplicationUser.cs
@@ -2,13 +2,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Domain.Models;
 
-public class ApplicationUser :  IdentityUser<Guid>
+public class ApplicationUser : IdentityUser<Guid>
 {
-    public string Name { get; set; }
-    
-    //Foreign Keys & Navigation Properties
-    public Guid? LocationId { get; set; }
-    public virtual Location Location { get; set; }
-    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
-    public virtual Cart Cart { get; set; }
+    public string Name { get; set; } = string.Empty;
 }
