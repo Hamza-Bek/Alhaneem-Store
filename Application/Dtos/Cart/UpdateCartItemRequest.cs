3041f8dd732c060b6733fcd5b9b3fc7580adb453
Hamza Bek <hamzamrbek@gmail.com>
2025-05-05 22:48:31 +0300

    add update cart item request

---
+++ Application/Dtos/Cart/UpdateCartItemRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos.Cart
+{
+    public class UpdateCartItemRequest
+    {
+        public Guid ProductId { get; set; }
+        public int QuantityDelta { get; set; } // +1 or -1
+        public string SessionId { get; set; } = string.Empty;
+    }
+}
