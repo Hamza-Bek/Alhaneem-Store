b2d215124f1aff8621831dbdb85063f7e064e6b3
Hamza Bek <hamzamrbek@gmail.com>
2025-05-05 22:50:54 +0300

    add IOrderService

---
+++ Application/Services/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using Application.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Service;
+
+public interface IOrderService
+{
+    Task<bool> SubmitOrderAsync(string sessionId);
+}
