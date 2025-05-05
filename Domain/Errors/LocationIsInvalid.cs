aa87703a6a12c77979b737a1f2c00afc74ea1e95
Hamza Bek <hamzamrbek@gmail.com>
2025-05-05 22:46:53 +0300

    add error model for location

---
+++ Domain/Errors/LocationIsInvalid.cs
@@ -0,0 +1,16 @@
+﻿using FluentResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Errors
+{
+    public class LocationIsInvalid : Error
+    {
+        public LocationIsInvalid() : base("Location is missed or invalid.")
+        {
+        }
+    }
+}
