9dfa0d1bd07f9ab7c86fc445bc27bf4963e2eb52
Hamza Bek <hamzamrbek@gmail.com>
2025-05-13 20:51:41 +0300

    add cartItemDto class

--- Application/Dtos/Cart/CartItemDto.cs
+++ Application/Dtos/Cart/CartItemDto.cs
@@ -1,8 +1,21 @@
+using Application.Dtos.Product;
+
 namespace Application.Dtos.Cart;
 
 public class CartItemDto
 {
+    public Guid Id { get; set; } // From EntityBase
+
     // Quantity Information
-    public int Quantity { get; set; } 
-    public Guid ProductId { get; set; } 
+    public int Quantity { get; set; }
+
+    // Pricing Information
+    public decimal Price { get; set; }
+    public decimal TotalPrice { get; set; }
+
+    // Product Info
+    public Guid ProductId { get; set; }
+    public string? ProductName { get; set; }
+    public string? ProductDescription { get; set; }
+    public List<string> ImageUrls { get; set; } = new();
 }
