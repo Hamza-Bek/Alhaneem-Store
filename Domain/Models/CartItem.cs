da1254e156a4ecb1d1d99a77dd89cb795f9c5e91
Hamza Bek <hamzamrbek@gmail.com>
2025-05-13 20:51:14 +0300

    add cartitem model

--- Domain/Models/CartItem.cs
+++ Domain/Models/CartItem.cs
@@ -3,8 +3,8 @@ namespace Domain.Models;
 public class CartItem : EntityBase
 {
     // Quantity Information
-    public int Quantity { get; set; } 
-    
+    public int Quantity { get; set; }
+
     // Pricing Information
     public decimal Price { get; set; }
     public decimal TotalPrice { get; set; }
@@ -12,7 +12,7 @@ public class CartItem : EntityBase
     //Foreign Keys & Navigation Properties
     public Guid CartId { get; set; }
     public virtual Cart Cart { get; set; }
-    
-    public Guid ProductId { get; set; } 
+
+    public Guid ProductId { get; set; }
     public virtual Product Product { get; set; }
 }
