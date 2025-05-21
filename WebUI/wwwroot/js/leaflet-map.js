9b6bb6e87be9725dff6c771fc1bab768a3db0759
Hamza Bek <hamzamrbek@gmail.com>
2025-05-21 20:38:17 +0300

    Add Js files

---
+++ WebUI/wwwroot/js/leaflet-map.js
@@ -0,0 +1,37 @@
+﻿let map, marker;
+
+window.initLeafletMap = function (dotNetHelper) {
+    map = L.map('map').setView([30.0444, 31.2357], 13); // Default center (Cairo)
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+    map.on('click', function (e) {
+        setMarker(e.latlng.lat, e.latlng.lng, dotNetHelper);
+    });
+};
+
+window.detectUserLocation = function (dotNetHelper) {
+    if (!navigator.geolocation) {
+        alert("Geolocation is not supported.");
+        return;
+    }
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+        map.setView([lat, lng], 13);
+        setMarker(lat, lng, dotNetHelper);
+    }, function (error) {
+        alert("Failed to detect your location.");
+        console.error(error);
+    });
+};
+
+function setMarker(lat, lng, dotNetHelper) {
+    if (!marker) {
+        marker = L.marker([lat, lng]).addTo(map);
+    } else {
+        marker.setLatLng([lat, lng]);
+    }
+    dotNetHelper.invokeMethodAsync('SetLocation', lat, lng);
+}
--- WebUI/wwwroot/js/script.js
+++ WebUI/wwwroot/js/script.js
@@ -32,43 +32,14 @@ new Swiper('.card-wrapper', {
     }
 });
 
-let map, marker;
-
-window.initLeafletMap = function (dotNetHelper) {
-    map = L.map('map').setView([30.0444, 31.2357], 13); // Default center (Cairo)
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-
-    map.on('click', function (e) {
-        setMarker(e.latlng.lat, e.latlng.lng, dotNetHelper);
+window.copyTextToClipboard = function (text) {
+    navigator.clipboard.writeText(text).then(function () {
+        console.log("Copied to clipboard:", text);
+    }, function (err) {
+        console.error("Failed to copy:", err);
     });
 };
 
-window.detectUserLocation = function (dotNetHelper) {
-    if (!navigator.geolocation) {
-        alert("Geolocation is not supported.");
-        return;
-    }
-
-    navigator.geolocation.getCurrentPosition(function (position) {
-        const lat = position.coords.latitude;
-        const lng = position.coords.longitude;
-        map.setView([lat, lng], 13);
-        setMarker(lat, lng, dotNetHelper);
-    }, function (error) {
-        alert("Failed to detect your location.");
-        console.error(error);
-    });
-};
-
-function setMarker(lat, lng, dotNetHelper) {
-    if (!marker) {
-        marker = L.marker([lat, lng]).addTo(map);
-    } else {
-        marker.setLatLng([lat, lng]);
-    }
-    dotNetHelper.invokeMethodAsync('SetLocation', lat, lng);
-}
 
 
 //window.showToast = function (message, type) {
